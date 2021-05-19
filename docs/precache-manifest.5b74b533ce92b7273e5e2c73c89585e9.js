93de8c98bc5794ba3960383f71fd0a41cdcc3b56
zhangchao <544262408@qq.com>
2021-05-19 15:36:20 +0800

    setup

---
+++ docs/precache-manifest.5b74b533ce92b7273e5e2c73c89585e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34326f1ce8eedb03f2d657adc8057158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "650a0a082b8f59872fda",
+    "url": "/static/js/2.fd5ba8fe.chunk.js"
+  },
+  {
+    "revision": "48dc2a0f18dfdbbcdcf81b94d8a3e1da",
+    "url": "/static/js/2.fd5ba8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37aacab73db707fb2f35",
+    "url": "/static/js/main.e3e16d77.chunk.js"
+  },
+  {
+    "revision": "d7b5075ecf70ba60e7e7",
+    "url": "/static/js/runtime-main.9cb2a799.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "7f795e8cf9d47425a145778d75db8931",
+    "url": "/static/media/foot.7f795e8c.png"
+  },
+  {
+    "revision": "160c23fcb969890fe166ae142609175c",
+    "url": "/static/media/logo.160c23fc.png"
+  }
+]);
